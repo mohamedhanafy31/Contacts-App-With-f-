2933dab15e5f7730007d532a94e971856c8f0ae3
Islam Adel <119044942+ISLAM2ADEL@users.noreply.github.com>
2024-12-13 23:31:51 +0200

    the implementation of StateService.fs

--- ConsoleApp4/StateService.fs
+++ ConsoleApp4/StateService.fs
@@ -1,3 +1,35 @@
-﻿module StateService
+namespace ContactManagement
 
-// ---- That module will contain (Add, Delete, Update) operations on the contact -----
+module StateService =
+    let updateContact (name: string) (updatedContact: Contact) (state: AppState) : Result<AppState, string> =
+        if state.Contacts.ContainsKey name then
+            Ok { state with Contacts = state.Contacts.Add(name, updatedContact) }
+        else
+            Error "Contact not found"
+
+
+    let addContact (contact: Contact) (state: AppState) : Result<AppState, string> =
+        if Validation.isValidContact contact then
+            if state.Contacts.ContainsKey contact.Name then
+                Error "A contact with this name already exists"
+            else
+                Ok { state with Contacts = state.Contacts.Add(contact.Name, contact) }
+        else
+            Error "Invalid contact details"
+
+
+    let deleteContact (name: string) (state: AppState) : AppState =
+        { state with Contacts = state.Contacts.Remove(name) }
+
+    let renameContact (oldName: string) (newName: string) (updatedContact: Contact) (state: AppState) : Result<AppState, string> =
+        if not (state.Contacts.ContainsKey oldName) then
+            Error "Original contact not found"
+        elif state.Contacts.ContainsKey newName then
+            Error "A contact with the new name already exists"
+        else
+            let updatedContacts = 
+                state.Contacts
+                |> Map.remove oldName
+                |> Map.add newName updatedContact
+            Ok { state with Contacts = updatedContacts }
+
