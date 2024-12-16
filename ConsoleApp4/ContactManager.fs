7a3a79f95cc8369b0db76f55e929c817f52f71c0
MOHAMED AHMED HANAFI <105173805+mohamedhanafy31@users.noreply.github.com>
2024-12-16 10:55:21 +0200

    Update ContactManager.fs
    Some Edits on GUI Design

--- ConsoleApp4/ContactManager.fs
+++ ConsoleApp4/ContactManager.fs
@@ -7,16 +7,20 @@ open System.Windows.Forms
 open System.Drawing
 open System.IO
 open System.Text.Json
-open DataTypes
-// Existing code for Domain Model, Validation, FileManager, etc.
+
 
 [<RequireQualifiedAccess>]
 module ContactManager =
-
+    
     // Helper function to create a labeled text box
     let createLabeledTextBox label top =
-        let itemLabel = new Label(Text = label, Top = top, Left = 10, Width = 100)
-        let itemTextBox = new TextBox(Top = top + 20, Left = 10, Width = 200)
+        let boldFont = new Font("Arial", 12.0f, FontStyle.Bold)
+        let itemLabel = new Label(Text = label, Top = top, Left = 10, Width = 100,Font = boldFont)
+        itemLabel.Font <- new Font("Segoe UI", 14.0f, FontStyle.Regular)
+
+        let itemTextBox = new TextBox(Top = top + 20, Left = 10, Width = 300)
+        itemTextBox.Font <- new Font("Open Sans", 12.0f, FontStyle.Regular)
+        
         (itemLabel :> Control, itemTextBox :> Control)
 
     // Function to show contact details
@@ -24,23 +28,19 @@ module ContactManager =
         let cardForm = 
             new Form(
                 Text = contact.Name, 
-                Width = 300, 
+                Width = 500, 
                 Height = 350, 
                 BackColor = ColorUtilities.hexToColor(contact.Color)
             )
         cardForm.StartPosition <- FormStartPosition.CenterParent
         
         let textColor = 
-            Validation.getTextColor (ColorUtilities.hexToColor(contact.Color))
+            ColorUtilities.getTextColor (ColorUtilities.hexToColor(contact.Color))
         
         let createLabel text top = 
-            new Label(
-                Text = text, 
-                Top = top, 
-                Left = 10, 
-                Width = 280, 
-                ForeColor = textColor
-            )
+            let label = new Label(Text = text, Top = top, Left = 10, Width = 350, ForeColor = textColor)
+            label.Font <- new Font("Segoe UI", 12.0f, FontStyle.Regular)
+            label
         
         let nameLabel = createLabel $"Name: {contact.Name}" 10
         let phoneLabel = createLabel $"Phone: {contact.PhoneNumber}" 40
@@ -49,16 +49,11 @@ module ContactManager =
         [nameLabel; phoneLabel; emailLabel] 
         |> List.iter cardForm.Controls.Add
         
-        // Edit and delete buttons
+        // Edit button
         let editButton = 
-            new Button(
-                Text = "Edit", 
-                Top = 250, 
-                Left = 110, 
-                Width = 80
-            )
-        
-        // Handle editing a contact
+            new Button(Text = "Edit", Top = 250, Left = 130, Width = 80, Height = 40, ForeColor = textColor)
+        editButton.Font <- new Font("Segoe UI", 14.0f, FontStyle.Bold)
+
         editButton.Click.Add (fun _ ->
             let editForm = 
                 new Form(
@@ -68,12 +63,9 @@ module ContactManager =
                     StartPosition = FormStartPosition.CenterParent
                 )
 
-            let (nameLabel, nameTextBox) = 
-                createLabeledTextBox "Name" 10
-            let (phoneLabel, phoneTextBox) = 
-                createLabeledTextBox "Phone (Digits Only)" 70
-            let (emailLabel, emailTextBox) = 
-                createLabeledTextBox "Email" 130
+            let (nameLabel, nameTextBox) = createLabeledTextBox "Name" 10
+            let (phoneLabel, phoneTextBox) = createLabeledTextBox "Phone (Digits Only)" 70
+            let (emailLabel, emailTextBox) = createLabeledTextBox "Email" 130
 
             nameTextBox.Text <- contact.Name
             phoneTextBox.Text <- contact.PhoneNumber
@@ -89,6 +81,15 @@ module ContactManager =
             
             let mutable selectedColor = ColorUtilities.hexToColor(contact.Color)
             
+            let colorPicker = 
+                new Button(
+                    Text = "Choose Color", 
+                    Top = 210, 
+                    Left = 10, 
+                    Width = 200,
+                    Height = 40
+                )
+                        
             colorPicker.Click.Add (fun _ ->
                 let colorDialog = new ColorDialog()
                 if colorDialog.ShowDialog() = DialogResult.OK then
@@ -98,12 +99,14 @@ module ContactManager =
             
             let saveButton = 
                 new Button(
-                    Text = "Save", 
-                    Top = 230, 
+                    Text = "\U0000270F", 
+                    Top = 260, 
                     Left = 10, 
-                    Width = 200
+                    Width = 200,
+                    Height = 40,
+                    BackColor = Color.LightGreen
                 )
-            
+
             saveButton.Click.Add (fun _ ->
                 let newContact = {
                     Name = nameTextBox.Text
@@ -122,7 +125,7 @@ module ContactManager =
                 | Error msg ->
                     MessageBox.Show(msg) |> ignore
             )
-            
+
             // Add controls to edit form
             editForm.Controls.AddRange([|
                 nameLabel :> Control; 
@@ -137,17 +140,12 @@ module ContactManager =
             
             editForm.ShowDialog() |> ignore
         )
-        
+
+        // Delete button
         let deleteButton = 
-            new Button(
-                Text = "Delete", 
-                Top = 250, 
-                Left = 200, 
-                Width = 80, 
-                BackColor = Color.LightCoral
-            )
-        
-        // Handle deleting a contact
+            new Button(Text = "Delete", Top = 250, Left = 250,  Width = 80, Height = 40 , BackColor = Color.Red)
+        deleteButton.Font <- new Font("Segoe UI", 14.0f, FontStyle.Bold)
+
         deleteButton.Click.Add (fun _ ->
             let result = 
                 MessageBox.Show(
@@ -164,22 +162,18 @@ module ContactManager =
                 refreshPanel()
                 cardForm.Close()
         )
-        
-        [editButton; deleteButton] 
-        |> List.iter cardForm.Controls.Add
-        
-        cardForm.ShowDialog() |> ignore
 
-    // Main UI form creation and layout
+        [editButton; deleteButton] |> List.iter cardForm.Controls.Add
+        cardForm.ShowDialog() |> ignore
     let createMainForm () =
         let form = 
             new Form(
                 Text = "Functional Contact Management", 
-                Width = 800, 
-                Height = 600
+                Width = 600, 
+                Height = 600,
+                Font = new Font("Segoe UI", 10.0f)
             )
         
-        // Add contact button and search panel
         let layout = 
             new TableLayoutPanel(
                 Dock = DockStyle.Top, 
@@ -188,8 +182,8 @@ module ContactManager =
             )
         
         layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20.0f))
-        layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40.0f))
-        layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40.0f))
+        layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50.0f))
+        layout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30.0f))
         layout.RowStyles.Add(new RowStyle(SizeType.Percent, 50.0f))
         layout.RowStyles.Add(new RowStyle(SizeType.Percent, 50.0f))
         
@@ -199,20 +193,24 @@ module ContactManager =
                 Width = 50, 
                 Height = 10, 
                 Dock = DockStyle.Right, 
-                Font = new Font(FontFamily.GenericSansSerif, 16.0f, FontStyle.Bold),
+                Font = new Font("Segoe UI", 16.0f, FontStyle.Bold),
                 BackColor = Color.LightBlue
             )
         
         let searchTextBox = 
             new TextBox(
                 Width = 200, 
-                Anchor = AnchorStyles.None
+                Height = 30,
+                Anchor = AnchorStyles.None,
+                Font = new Font("Segoe UI", 10.0f)
             )
         
         let searchButton = 
             new Button(
-                Text = "Search", 
-                Width = 80
+                Text = "\U0001F50D", 
+                Width = 30,
+                Height = 30,
+                Font = new Font("Segoe UI", 10.0f)
             )
         
         let searchPanel = 
@@ -223,8 +221,8 @@ module ContactManager =
                 Margin = Padding(0, 20, 0, 0)
             )
         
-        searchPanel.Controls.Add(searchButton)
         searchPanel.Controls.Add(searchTextBox)
+        searchPanel.Controls.Add(searchButton)
         
         layout.Controls.Add(new Label(), 0, 0)
         layout.Controls.Add(searchPanel, 1, 0)
@@ -237,15 +235,14 @@ module ContactManager =
                 Padding = Padding(10)
             )
         
-        // State initialization
         let initialState = {
             Contacts = FileManager.loadContacts()
             SearchQuery = None
         }
         
         let state = ref initialState
-        
-        // Refresh contact panel based on search query
+
+        // Function to refresh contact panel
         let rec refreshContactPanel (searchQuery: string option) =
             contactPanel.Controls.Clear()
             
@@ -265,7 +262,7 @@ module ContactManager =
             displayContacts 
             |> List.iter (fun (_, contact) ->
                 let textColor = 
-                    Validation.getTextColor (ColorUtilities.hexToColor(contact.Color))
+                    ColorUtilities.getTextColor (ColorUtilities.hexToColor(contact.Color))
                 
                 let button = 
                     new Button(
@@ -273,7 +270,8 @@ module ContactManager =
                         Width = 200, 
                         Height = 30, 
                         BackColor = ColorUtilities.hexToColor(contact.Color), 
-                        ForeColor = textColor
+                        ForeColor = textColor,
+                        Font = new Font("Segoe UI", 10.0f, FontStyle.Bold)
                     )
                 
                 button.Click.Add(fun _ -> 
@@ -283,67 +281,65 @@ module ContactManager =
                 contactPanel.Controls.Add(button)
             )
         
-        // Search button click
+        // Search button functionality
         searchButton.Click.Add (fun _ ->
             let query = searchTextBox.Text
             refreshContactPanel (if String.IsNullOrWhiteSpace(query) then None else Some query)
         )
         
-        // Add button click
+        // Add button functionality
         addButton.Click.Add (fun _ ->
             let popupForm = 
                 new Form(
                     Text = "Add Contact", 
-                    Width = 400, 
+                    Width = 350, 
                     Height = 350, 
                     StartPosition = FormStartPosition.CenterParent
                 )
-
-            let nameControls = createLabeledTextBox "Name" 10 |> fun (l, t) -> [l; t]
-            let phoneControls = createLabeledTextBox "Phone (Digits Only)" 70 |> fun (l, t) -> [l; t]
-            let emailControls = createLabeledTextBox "Email" 130 |> fun (l, t) -> [l; t]
-
+            
+            let (nameLabel, nameTextBox) = createLabeledTextBox "Name" 10
+            let (phoneLabel, phoneTextBox) = createLabeledTextBox "Phone" 70
+            let (emailLabel, emailTextBox) = createLabeledTextBox "Email" 130
 
             let colorPicker = 
                 new Button(
                     Text = "Choose Color", 
-                    Top = 190, 
+                    Top = 210, 
                     Left = 10, 
-                    Width = 200
+                    Width = 200,
+                    Height = 40
                 )
-
+            
             let mutable selectedColor = Color.LightGray
-
+            
             colorPicker.Click.Add (fun _ ->
                 let colorDialog = new ColorDialog()
                 if colorDialog.ShowDialog() = DialogResult.OK then
                     selectedColor <- colorDialog.Color
                     colorPicker.BackColor <- selectedColor
             )
-
+            
             let saveButton = 
                 new Button(
-                    Text = "Save", 
-                    Top = 230, 
+                    Text = "\U0000270F", 
+                    Top = 260, 
                     Left = 10, 
-                    Width = 200
+                    Width = 200,
+                    Height = 40,
+                    BackColor = Color.LightGreen
                 )
 
-            // Get textboxes from controls
-            let getTextBox (controls: Control list) = 
-                controls 
-                |> List.pick (function 
-                    | :? TextBox as tb -> Some tb 
-                    | _ -> None)
-
+            colorPicker.Font <- new Font("Segoe UI", 14.0f, FontStyle.Bold)
+            saveButton.Font <- new Font("Segoe UI", 14.0f, FontStyle.Bold)
+            
             saveButton.Click.Add (fun _ ->
                 let newContact = {
-                    Name = (getTextBox nameControls).Text
-                    PhoneNumber = (getTextBox phoneControls).Text
-                    Email = (getTextBox emailControls).Text
+                    Name = nameTextBox.Text
+                    PhoneNumber = phoneTextBox.Text
+                    Email = emailTextBox.Text
                     Color = ColorUtilities.colorToHex(selectedColor)
                 }
-
+                
                 match StateService.addContact newContact !state with
                 | Ok newState ->
                     state := newState
@@ -353,18 +349,24 @@ module ContactManager =
                 | Error msg ->
                     MessageBox.Show(msg) |> ignore
             )
-
-            // Add controls to form
-            (nameControls @ phoneControls @ emailControls @ [colorPicker; saveButton])
-            |> List.iter popupForm.Controls.Add
-
+            
+            popupForm.Controls.AddRange([|
+                nameLabel :> Control; 
+                nameTextBox :> Control; 
+                phoneLabel :> Control; 
+                phoneTextBox :> Control; 
+                emailLabel :> Control; 
+                emailTextBox :> Control; 
+                colorPicker :> Control; 
+                saveButton :> Control
+            |])
+            
             popupForm.ShowDialog() |> ignore
         )
 
-        // Initial panel refresh
+        // Initial load of contacts
         refreshContactPanel None
         
-        // Add components to the form
         form.Controls.Add(contactPanel)
         form.Controls.Add(layout)
         
