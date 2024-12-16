80081958b7325d263f821209bc15cc92914936a7
MOHAMED AHMED HANAFI <105173805+mohamedhanafy31@users.noreply.github.com>
2024-12-16 10:51:55 +0200

    Update ColorUtilities.fs
    Adding more color functions

--- ConsoleApp4/ColorUtilities.fs
+++ ConsoleApp4/ColorUtilities.fs
@@ -1,10 +1,20 @@
-﻿namespace ContactManagement
+namespace ContactManagement
 
 open System.Drawing
 open System
-open DataTypes
-
 module ColorUtilities =
+    
+    // Shared luminance calculation
+    let calculateLuminance (color: Color) =
+        (0.299 * float color.R + 0.587 * float color.G + 0.114 * float color.B) / 255.0
+
+    let getTextColor (bgColor: Color) =
+        if calculateLuminance bgColor > 0.5 then Color.Black else Color.White
+
+    let getContrastColor (bgColor: Color) =
+        let luminance = calculateLuminance bgColor
+        if luminance > 0.5 then Color.Black else Color.White
+
     let colorToHex (color: Color) =
         System.Drawing.ColorTranslator.ToHtml(color)
 
@@ -14,6 +24,4 @@ module ColorUtilities =
         with
         | :? ArgumentException -> failwith "Invalid color hex string"
 
-    let getContrastColor (bgColor: Color) =
-        let luminance = Validation.calculateLuminance bgColor
-        if luminance > 0.5 then Color.Black else Color.White
+
